18779ed73adb80b2f9a80c582f4fecb8c114b704
PedroHenriqueDiasBatista <phdiasbatistaprofessional@gmail.com>
2022-11-06 18:45:21 -0300

    feat: create DBException class

---
+++ src/main/java/com/tcc/easyjobgo/exception/DBException.java
@@ -0,0 +1,13 @@
+package com.tcc.easyjobgo.exception;
+
+public class DBException  extends RuntimeException{
+
+    public DBException(String message) {
+        super(message);
+    }
+
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
