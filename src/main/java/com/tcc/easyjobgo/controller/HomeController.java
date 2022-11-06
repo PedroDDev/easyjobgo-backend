022466bc00b53c80458b291af49fa72781b4459e
PedroHenriqueDiasBatista <phdiasbatistaprofessional@gmail.com>
2022-11-06 18:47:44 -0300

    feat: create HomeController controller

---
+++ src/main/java/com/tcc/easyjobgo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.tcc.easyjobgo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping(path = "easyjobgo/v1")
+public class HomeController {
+    
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    @GetMapping(value="/home")
+    public String getStart() {
+        return "Bem vindo(a)!";
+    }
+}
