95690ae96e12cfbb8a2ea6edab4f9a5fd9814a15
PedroHenriqueDiasBatista <phdiasbatistaprofessional@gmail.com>
2022-11-06 18:53:29 -0300

    feat(security): create CustomAuthenticationFilter class

---
+++ src/main/java/com/tcc/easyjobgo/config/security/CustomAuthenticationFilter.java
@@ -0,0 +1,81 @@
+package com.tcc.easyjobgo.config.security;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    final AuthenticationManager authManager;
+
+    public CustomAuthenticationFilter (AuthenticationManager authManager)
+    {
+        this.authManager = authManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        logger.info("Username is: " + username); logger.info("Password is: " + password);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        User user = (User)auth.getPrincipal();
+
+        Algorithm alg = Algorithm.HMAC256("secret".getBytes());
+
+        String accessToken = JWT.create()
+                                .withSubject(user.getUsername())
+                                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                                .withIssuer(request.getRequestURL().toString())
+                                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                                .sign(alg);
+        String refreshToken = JWT.create()
+                                .withSubject(user.getUsername())
+                                .withExpiresAt(new Date(System.currentTimeMillis() + 45 * 60 * 1000))
+                                .withIssuer(request.getRequestURL().toString())
+                                .sign(alg);
+
+        // response.setHeader("accessToken", accessToken);
+        // response.setHeader("refreshToken", refreshToken);
+
+        Map<String, String> credentials = new HashMap<>();
+
+        credentials.put("username", user.getUsername());
+        credentials.put("accessToken", accessToken);
+        credentials.put("refreshToken", refreshToken);
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), credentials);
+        
+    }
+    
+
+}
