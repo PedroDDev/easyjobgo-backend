122da38829929406588304908836e92c864f8397
PedroHenriqueDiasBatista <phdiasbatistaprofessional@gmail.com>
2022-11-06 18:43:16 -0300

    feat: create ServiceCategory model

---
+++ src/main/java/com/tcc/easyjobgo/model/ServiceCategory.java
@@ -0,0 +1,28 @@
+package com.tcc.easyjobgo.model;
+
+public class ServiceCategory {
+
+    private Integer id;
+    private String description;
+    
+    public ServiceCategory(){
+        
+    }
+    public ServiceCategory(Integer id, String description){
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    
+}
